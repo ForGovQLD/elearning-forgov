f5c5559fa71ceb09f362e87887f5152c3034d744
Jordan Quigg <jordan.quigg@chde.qld.gov.au>
2025-11-13 12:35:47 +1000

    Add PSC collaboration tools modules

---
+++ modules/PSC_collaboration_tools/Cross-agency_Collaboration_Navigator/story_content/user.js
@@ -0,0 +1,168 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  var player = GetPlayer();
+const Q1response = player.GetVar("Q1response");
+const Q2response = player.GetVar("Q2response");
+const Q3response = player.GetVar("Q3response");
+const Q4response = player.GetVar("Q4response");
+const Q5response = player.GetVar("Q5response");
+const Q6response = player.GetVar("Q6response");
+const Q7response = player.GetVar("Q7response");
+
+Constructpdf();
+
+async function Constructpdf() {
+  try {
+    await loadMods();
+    await fillForm();
+  } catch (err) {
+    console.error("Error in Constructpdf:", err);
+  }
+}
+
+async function loadMods() {
+  try {
+    await import("https://unpkg.com/pdf-lib/dist/pdf-lib.js");
+  } catch (err) {
+    console.error("Error loading pdf-lib:", err);
+  }
+}
+
+async function fillForm() {
+  try {
+    const { PDFDocument } = PDFLib;
+    const formUrl = 'Navigator_Full Collaboration result.pdf';
+    const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer());
+    const pdfDoc = await PDFDocument.load(formPdfBytes);
+    const form = pdfDoc.getForm();
+
+    // Get field names (fields are named exactly as Storyline variables)
+    const Q1responseField = form.getTextField('Q1responseField');
+    const Q2responseField = form.getTextField('Q2responseField');
+    const Q3responseField = form.getTextField('Q3responseField');
+    const Q4responseField = form.getTextField('Q4responseField');
+    const Q5responseField = form.getTextField('Q5responseField');
+    const Q6responseField = form.getTextField('Q6responseField');
+    const Q7responseField = form.getTextField('Q7responseField');
+
+    // Fill in form fields
+    Q1responseField.setText(Q1response);
+    Q2responseField.setText(Q2response);
+    Q3responseField.setText(Q3response);
+    Q4responseField.setText(Q4response);
+    Q5responseField.setText(Q5response);
+    Q6responseField.setText(Q6response);
+    Q7responseField.setText(Q7response);
+
+    // Flatten the form fields
+    form.flatten();
+
+    // Save and download the form
+    const pdfBytes = await pdfDoc.save();
+    const filename = "Navigator_Full Collaboration result.pdf"; // Use a static filename
+    await import('https://unpkg.com/tiny-save-as/dist/tiny-save-as.esm.js')
+      .then(({ default: saveAs }) => {
+        const blob = new Blob([pdfBytes], { type: 'application/octet-stream' });
+        saveAs(blob, filename);
+      })
+      .catch((err) => {
+        console.error("Error saving the file:", err);
+      });
+  } catch (err) {
+    console.error("Error in fillForm:", err);
+  }
+}
+}
+
+window.Script2 = function()
+{
+  var player = GetPlayer();
+const Q1response = player.GetVar("Q1response");
+const Q2response = player.GetVar("Q2response");
+const Q3response = player.GetVar("Q3response");
+const Q4response = player.GetVar("Q4response");
+const Q5response = player.GetVar("Q5response");
+const Q6response = player.GetVar("Q6response");
+const Q7response = player.GetVar("Q7response");
+
+Constructpdf();
+
+async function Constructpdf() {
+  try {
+    await loadMods();
+    await fillForm();
+  } catch (err) {
+    console.error("Error in Constructpdf:", err);
+  }
+}
+
+async function loadMods() {
+  try {
+    await import("https://unpkg.com/pdf-lib/dist/pdf-lib.js");
+  } catch (err) {
+    console.error("Error loading pdf-lib:", err);
+  }
+}
+
+async function fillForm() {
+  try {
+    const { PDFDocument } = PDFLib;
+    const formUrl = 'Navigator_Full Collaboration result.pdf';
+    const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer());
+    const pdfDoc = await PDFDocument.load(formPdfBytes);
+    const form = pdfDoc.getForm();
+
+    // Get field names (fields are named exactly as Storyline variables)
+    const Q1responseField = form.getTextField('Q1responseField');
+    const Q2responseField = form.getTextField('Q2responseField');
+    const Q3responseField = form.getTextField('Q3responseField');
+    const Q4responseField = form.getTextField('Q4responseField');
+    const Q5responseField = form.getTextField('Q5responseField');
+    const Q6responseField = form.getTextField('Q6responseField');
+    const Q7responseField = form.getTextField('Q7responseField');
+
+    // Fill in form fields
+    Q1responseField.setText(Q1response);
+    Q2responseField.setText(Q2response);
+    Q3responseField.setText(Q3response);
+    Q4responseField.setText(Q4response);
+    Q5responseField.setText(Q5response);
+    Q6responseField.setText(Q6response);
+    Q7responseField.setText(Q7response);
+
+    // Flatten the form fields
+    form.flatten();
+
+    // Save and download the form
+    const pdfBytes = await pdfDoc.save();
+    const filename = "Navigator_Full Collaboration result.pdf"; // Use a static filename
+    await import('https://unpkg.com/tiny-save-as/dist/tiny-save-as.esm.js')
+      .then(({ default: saveAs }) => {
+        const blob = new Blob([pdfBytes], { type: 'application/octet-stream' });
+        saveAs(blob, filename);
+      })
+      .catch((err) => {
+        console.error("Error saving the file:", err);
+      });
+  } catch (err) {
+    console.error("Error in fillForm:", err);
+  }
+}
+}
+
+};
---
+++ modules/PSC_collaboration_tools/Cross-agency_Collaboration_Pulse_Check/story_content/triggers.js
@@ -0,0 +1,215 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6WcxVaJWaTr":
+        Script1();
+        break;
+      case "6QHNKhSnRQk":
+        Script2();
+        break;
+      case "6ovU9B09plv":
+        Script3();
+        break;
+      case "6Wgu2f0QbK1":
+        Script4();
+        break;
+      case "5o1vbcmr05V":
+        Script5();
+        break;
+      case "6W2zvjOQwS7":
+        Script6();
+        break;
+      case "6qWLWcxDSba":
+        Script7();
+        break;
+      case "6haVEFGBtvK":
+        Script8();
+        break;
+      case "6Oju5etwmnm":
+        Script9();
+        break;
+      case "64xQVleQExw":
+        Script10();
+        break;
+      case "5qph8m1SFdj":
+        Script11();
+        break;
+      case "6nnuRQiq0tN":
+        Script12();
+        break;
+      case "5j5Dz2a877Z":
+        Script13();
+        break;
+      case "6KNvTfs9ugi":
+        Script14();
+        break;
+      case "5eVaVmuxWdS":
+        Script15();
+        break;
+      case "6aud6xMI2XI":
+        Script16();
+        break;
+      case "6N3Y31t33VZ":
+        Script17();
+        break;
+      case "6rAtTyieCWM":
+        Script18();
+        break;
+      case "5WDqtq3fnvg":
+        Script19();
+        break;
+      case "5YmoeEQtE2i":
+        Script20();
+        break;
+      case "6GTERnkv5qq":
+        Script21();
+        break;
+      case "5fImnrtg1yE":
+        Script22();
+        break;
+      case "69mdZNS1q6M":
+        Script23();
+        break;
+      case "5VzWPjPtn7f":
+        Script24();
+        break;
+      case "63SWtcZg7tT":
+        Script25();
+        break;
+      case "6cGa3SVhZYi":
+        Script26();
+        break;
+      case "61V8Za762tl":
+        Script27();
+        break;
+      case "5p84Rld8Uzs":
+        Script28();
+        break;
+      case "5ts4NE62m6M":
+        Script29();
+        break;
+      case "5ZpXC82SgNj":
+        Script30();
+        break;
+      case "6SmVZ3LIuZH":
+        Script31();
+        break;
+      case "6c33TDFVyXU":
+        Script32();
+        break;
+      case "5tgvJVTKdjX":
+        Script33();
+        break;
+      case "5q3Uwrfb7eC":
+        Script34();
+        break;
+      case "6WNpd9sVpwS":
+        Script35();
+        break;
+      case "6Og9kOxclbz":
+        Script36();
+        break;
+      case "6fXcZPdn6N8":
+        Script37();
+        break;
+      case "6OVk4Y0QGvV":
+        Script38();
+        break;
+      case "6MqCPYL4iVJ":
+        Script39();
+        break;
+      case "638eA81DmAK":
+        Script40();
+        break;
+      case "651nhT09Xhq":
+        Script41();
+        break;
+      case "6Jfk3ebkxw7":
+        Script42();
+        break;
+      case "5lLNauBedwD":
+        Script43();
+        break;
+      case "5r1GkFUhdYS":
+        Script44();
+        break;
+      case "686l5lReZFb":
+        Script45();
+        break;
+      case "6SUFqyn50jX":
+        Script46();
+        break;
+      case "5lelntPKfp0":
+        Script47();
+        break;
+      case "6ZQvbzzbbju":
+        Script48();
+        break;
+      case "6eCphAuwKHb":
+        Script49();
+        break;
+      case "6GzlBo67Rsr":
+        Script50();
+        break;
+      case "5oKJiIxthVw":
+        Script51();
+        break;
+      case "6AEQjwcE0Ll":
+        Script52();
+        break;
+      case "6ObxY3M5J91":
+        Script53();
+        break;
+      case "5s2GOaJZjG3":
+        Script54();
+        break;
+      case "6QQsKt0m4UD":
+        Script55();
+        break;
+      case "6gfp5kfUeal":
+        Script56();
+        break;
+      case "6g6kbgQdb5I":
+        Script57();
+        break;
+      case "658zqdlBdtC":
+        Script58();
+        break;
+      case "6OXQ8NqmdOg":
+        Script59();
+        break;
+      case "6fQTnVvYO0X":
+        Script60();
+        break;
+      case "5lovJLKSWCf":
+        Script61();
+        break;
+      case "5yE1UGs0AKX":
+        Script62();
+        break;
+      case "6VApGZjzY04":
+        Script63();
+        break;
+      case "5jnuPUP9uaO":
+        Script64();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
