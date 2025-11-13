b0b7b732ecc5a6e4c2dc44cbb7a26fca5e36208e
Jordan Quigg <jordan.quigg@chde.qld.gov.au>
2025-11-13 18:09:11 +1000

    Add final versions of PSC collaboration modules

---
+++ modules/PSC_collaboration_tools/Cross-agency_Collaboration_Navigator/story_content/triggers.js
@@ -0,0 +1,53 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ic0at6LoG2":
+        Script1();
+        break;
+      case "6NukcOVC0it":
+        Script2();
+        break;
+      case "5uqjAazIBb2":
+        Script3();
+        break;
+      case "5dzUSlE9hPe":
+        Script4();
+        break;
+      case "6b98el13ds9":
+        Script5();
+        break;
+      case "6gMi8jIwO87":
+        Script6();
+        break;
+      case "6F1QHnHYMQg":
+        Script7();
+        break;
+      case "6gXOA5qh8CV":
+        Script8();
+        break;
+      case "6Y8EplNprcY":
+        Script9();
+        break;
+      case "5tu53pnQqbI":
+        Script10();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
